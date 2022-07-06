2363531d38155d8a6e7fb59ded432d0c90de7c87
zven <andzven@gmail.com>
2022-07-06 18:58:55 +0800

    let's make it begin :tada:

---
+++ lib/buffered_task.ex
@@ -0,0 +1,18 @@
+defmodule BufferedTask do
+  @moduledoc """
+  Documentation for `BufferedTask`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BufferedTask.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/buffered_task_test.exs
@@ -0,0 +1,8 @@
+defmodule BufferedTaskTest do
+  use ExUnit.Case
+  doctest BufferedTask
+
+  test "greets the world" do
+    assert BufferedTask.hello() == :world
+  end
+end
